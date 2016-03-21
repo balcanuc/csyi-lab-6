b73cc7c43859606c3748379b13bad4295bc3797b
Cristian Balcanu <balcanu@amazon.com>
2016-03-21 21:38:21 +0200

    bundle the jar too...

--- codedeploy/scripts/application-stop.sh
+++ codedeploy/scripts/application-stop.sh
@@ -3,5 +3,5 @@
 echo "echo application-stop" >> /tmp/csyi_log.txt
 echo "working directory = $(pwd)" >> /tmp/csyi_log.txt
 echo "date = $(date)" >> /tmp/csyi_log.txt
-echo "whoami" >> /tmp/csyi_log.txt
+echo "whoami = $(whoami)" >> /tmp/csyi_log.txt
 echo >> /tmp/csyi_log.txt
