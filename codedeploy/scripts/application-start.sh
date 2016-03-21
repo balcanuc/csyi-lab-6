3833bb87cdc0778314cbf8ba9046ddcdab9b8879
Cristian Balcanu <balcanu@amazon.com>
2016-03-22 01:26:14 +0200

    switch from sleep to java

--- codedeploy/scripts/application-start.sh
+++ codedeploy/scripts/application-start.sh
@@ -6,8 +6,8 @@ echo "date = $(date)" >> /tmp/csyi_log.txt
 echo "whoami = $(whoami)" >> /tmp/csyi_log.txt
 echo >> /tmp/csyi_log.txt
 
-#nohup java -jar /tmp/cloudshape-1.0.0.jar > /dev/null 2> /dev/null < /dev/null &
-sleep 60 > /dev/null 2> /dev/null < /dev/null &
+java -jar /tmp/cloudshape-1.0.0.jar > /dev/null 2> /dev/null < /dev/null &
+# sleep 60 > /dev/null 2> /dev/null < /dev/null &
 server_pid=$!
 echo "Server PID=$server_pid"
 echo -n $server_pid > /tmp/csyi.pid
