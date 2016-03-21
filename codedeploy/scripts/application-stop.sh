ba01a229164ab0c5ea5383a88e1ae55568b799ac
Cristian Balcanu <balcanu@amazon.com>
2016-03-21 21:56:49 +0200

    make cloudshape application start and stop

--- codedeploy/scripts/application-stop.sh
+++ codedeploy/scripts/application-stop.sh
@@ -5,3 +5,8 @@ echo "working directory = $(pwd)" >> /tmp/csyi_log.txt
 echo "date = $(date)" >> /tmp/csyi_log.txt
 echo "whoami = $(whoami)" >> /tmp/csyi_log.txt
 echo >> /tmp/csyi_log.txt
+
+
+kill_pid=$(cat /tmp/csyi.pid)
+echo "killing PID $kill_pid"
+kill $kill_pid
--- codedeploy/scripts/before-install.sh
+++ codedeploy/scripts/before-install.sh
@@ -5,3 +5,9 @@ echo "working directory = $(pwd)" >> /tmp/csyi_log.txt
 echo "date = $(date)" >> /tmp/csyi_log.txt
 echo "whoami = $(whoami)" >> /tmp/csyi_log.txt
 echo >> /tmp/csyi_log.txt
+
+
+java -jar ./cloudshape-1.0.0.jar&
+server_pid=$!
+echo "Server PID=$server_pid"
+echo -n $server_pid > /tmp/csyi.pid
